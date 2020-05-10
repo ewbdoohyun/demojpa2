0dfab75a1991045df87e6a0feab669801d090b28
danny <doohyun.kim@mytrum.com>
2020-05-10 17:47:36 +0900

    Initial commit

---
+++ src/main/java/me/danny/demojpa2/Demojpa2Application.java
@@ -0,0 +1,14 @@
+package me.danny.demojpa2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demojpa2Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Demojpa2Application.class, args);
+    }
+
+}
---
+++ src/test/java/me/danny/demojpa2/Demojpa2ApplicationTests.java
@@ -0,0 +1,14 @@
+package me.danny.demojpa2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demojpa2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
